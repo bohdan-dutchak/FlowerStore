7df1669a9323f0e0c15708b8f7101401ae4dd692
Bohdan <bogdandutchak02@gmail.com>
2021-10-25 18:42:26 +0300

    Added CI/CD

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,13 +12,18 @@ public class main {
 
     }
 
+    public static double randomDouble(double l, double r){
+        double result = l + new Random().nextDouble() * (r - l);
+        return Math.round(result*100)/100;
+    }
+
     public static List<Flower> generateAssort(int n){
         List<Flower> flowers = new ArrayList<Flower>();
         Random random = new Random();
         final List<FlowerType> FT = Collections.unmodifiableList(Arrays.asList(FlowerType.values()));
         final List<Color> COLORS = Collections.unmodifiableList(Arrays.asList(Color.values()));
         for (int i = 0; i<n;i++){
-            Flower flower = new Flower(FT.get(random.nextInt(FT.size())),COLORS.get(random.nextInt(COLORS.size())), Math.round(random.nextDouble(1.0,10.0)*100.0)/100.0, Math.round( random.nextDouble(10,200)*100.0)/100.0);
+            Flower flower = new Flower(FT.get(random.nextInt(FT.size())),COLORS.get(random.nextInt(COLORS.size())), randomDouble(1,10), randomDouble(10,200));
             flowers.add(flower);
         }
         return flowers;
